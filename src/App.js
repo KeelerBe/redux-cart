5c0dcedd0fac983b98fe3f70a9c64a47d8d0d3eb
Keeler Mendoza <keelerbe@gmail.com>
2018-10-22 14:18:51 +0800

    Set up mock data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
-// import React from 'react'
+import React from 'react'
 import './App.css'
+import Products from './components/Products'
+import Cart from './components/Cart'
+import Inventory from './components/Inventory'
+import Orders from './components/Orders'
 
-const App = () => null
+const App = () => (
+	<div id="main">
+		<Products />
+		<Inventory />
+		<Cart />
+		<Orders />
+	</div>
+)
 
 export default App
---
+++ src/components/Inventory.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Inventory = () => (
+	<div id="inventory">
+		<h2>Inventory</h2>
+	</div>
+)
+
+export default Inventory

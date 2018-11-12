e1c33be49264e63f5533a506cf28b8c99f8b974a
Keeler Mendoza <keelerbe@gmail.com>
2018-11-12 08:38:54 +0800

    ...

--- src/containers/ProductFormContainer.js
+++ src/containers/ProductFormContainer.js
@@ -1,13 +1,15 @@
+import React, { Fragment } from 'react'
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
 import { startAddNewProduct } from '../actions'
 
-const ProductFormContainer = ({ startAddNewProduct, children }) =>
-	children(startAddNewProduct)
+const ProductFormContainer = ({ startAddNewProduct, children }) => (
+	<Fragment>{children(startAddNewProduct)}</Fragment>
+)
 
 ProductFormContainer.propTypes = {
 	startAddNewProduct: PropTypes.func.isRequired,
-	children: PropTypes.node.isRequired
+	children: PropTypes.func.isRequired
 }
 
 export default connect(
